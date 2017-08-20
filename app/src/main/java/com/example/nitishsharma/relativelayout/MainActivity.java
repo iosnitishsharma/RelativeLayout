d3fff365e8244b1f02e90eff7d36499c1cc3ef10
Nitish Sharma <nitishsharma@Nitishs-MacBook-Air.local>
2017-08-20 23:13:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nitishsharma/relativelayout/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.nitishsharma.relativelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // specify where the view will be placed with repect to other view or parent
+    // layout weight and layout gravity does not exist in relative layout
+
+
+
+    ///// place the vie in center of relative layout
+    ////,,, center_in_parent,,,,center_horizontal,,,center_vertical
+
+    ///align parenttop
+    //align parentleft
+    //align parent bottom
+    ///align parent right
+
+
+
+    ///define relation betwwen 2 views
+
+    //below above left right
+
+    //aligntop // alig bottom//align left //alugn right //align baseline
+
+
+    /// when you have specific reuirements like you want to show somehing based on user events then you
+    // have to build layout programattically
+
+
+
+    //create all view and view group objects
+    //define width and height for each
+    // define rule for each
+    //set additional proerties
+
+    // add view to the relative layout
+    //set content view as the relative layout
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
